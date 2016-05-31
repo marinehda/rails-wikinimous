f9b310f98cf397669a5783a0931fa9d182265fd6
Marine Houze <m.houzedelaulnoit@gmail.com>
2016-05-31 16:09:22 +0200

    exo2

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require "faker"
+
+10.times do
+  article = Article.new(
+    title: Faker::Lorem.sentence,
+    content: Faker::Lorem.paragraph
+  )
+  article.save
+end
+
+# Article.delete_all
+
+# Article.create(
+#   id: 1,
+#   title: "My Very First Post",
+#   content:
+#   %Q{### There Is Something You Should Know!
+
+#   This is my very first post using markdown!
+
+#   How do you like it?  I learned this from [RichOnRails.com](http://richonrails.com/articles/rendering-markdown-with-redcarpet)!}
+# )
+
+# Article.create(
+#   id: 2,
+#   title: "My Second Post",
+#   content:
+#   %Q{### My List of Things To Do!
+
+#   Here is the list of things I wish to do!
+
+#   * write more posts
+#   * write even more posts
+#   * write even more posts!}
+# )
